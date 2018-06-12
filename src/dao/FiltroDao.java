4dcd7bb805d03903f6b6735cdb5b7ff8569474f5
LN710Q <LN710Q@DEILABO1-PC18>
2018-06-12 12:46:51 -0600

    INVENTARIO

---
+++ src/dao/FiltroDao.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import conexion.Conexion;
+import interfaces.metodos;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.Filtro;
+
+/**
+ *
+ * @author LN710Q
+ */
+public class FiltroDao implements metodos<Filtro> {
+
+    private static final String SQL_INSERT = "INSERT INTO filtros_aceite(codFiltro, marca, stock, existencia) VALUES (?,?,?,?)";
+    private static final String SQL_UPDATE = "UPDATE filtros_aceite SET marca=?, stock=?, exitencia=? WHERE codFiltro=?";
+    private static final String SQL_DELETE = "DELETE FROM filtros_aceite WHERE codFiltros=?";
+    private static final String SQL_READ = " SELECT *FROM filtros_aceite WHERE codFiltro=?";
+    private static final String SQL_READALL = "SELECT *FROM filtros_aceite";
+    private static final Conexion con = Conexion.conectar();
+
+    @Override
+    public boolean create(Filtro g) {
+        PreparedStatement ps;
+        try {
+            ps = con.getCnx().prepareStatement(SQL_INSERT);
+            ps.setString(1, g.getCodigo());
+            ps.setString(2, g.getMarca());
+            ps.setInt(3, g.getStock());
+            ps.setBoolean(4, true);
+            if (ps.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            Logger.getLogger(FiltroDao.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            con.cerrarConexion();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(Object key) {
+        preparedStatement ps;
+        try {
+            ps = con.getCnx().prepareStatement(SQL_DELETE);
+            ps.setString(1, key.toString());
+
+            if (ps.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            Logger.getLogger(FiltroDao.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            con.cerrarConexion();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean update(Filtro c) {
+        PreparedStatement ps;
+        try {
+            System.out.println(c.getCodigo());
+            ps = con.getCnx().clientPrepareStatement(SQL_UPDATE);
+            ps.setString(1, c.getMarca());
+            ps.setInt(2, c.getStock());
+            ps.setBoolean(3, c.getExistencia());
+            ps.setString(4, c.getCodigo());
+            if (ps.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            Logger.getLogger(FiltroDao.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            con.cerrarConexion();
+        }
+        return false;
+    }
+
+    @Override
+    public Filtro read(Object key) {
+        Filtro f=null;
+        PreparedStatement ps;
+        ResultSet rs;
+        
+        try{
+            ps= con.getCnx().clientPrepareStatement(SQL_READ);
+            ps.setString(1, key.tosString());
+            
+            rs=ps.executeQuery();
+            
+            while(rs.next()){
+                f= new Filtro(rs.getInt(l),rs.getString(2),rs.getString(3),rs.getInt(4), rs.getBoolean(5));   
+            }
+            rs.close();
+        }
+        catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public ArrayList<Filtro> readAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}   
+
---
+++ src/modelo/Filtro.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author LN710Q
+ */
+public class Filtro {
+    private int id;
+    private String codigo;
+    private String marca;
+    private int stock;
+    private boolean existencia;
+    
+    public Filtro(){
+    }
+    
+    public Filtro(int id, String codigo, String marca, int stock, boolean exitencia){
+        this.id=id;
+        this.marca=marca;
+        this.stock=stock;
+        this.existencia=exitencia;
+        this.codigo=codigo;
+    }
+    
+    public Filtro(String codigo, String marca, int stock, boolean exitencia){
+        this.codigo=codigo;
+        this.marca=marca;
+        this.stock=stock;
+        this.existencia=existencia;
+    }
+    
+    public Filtro(String marca, int stock, boolean existencia){
+        this.marca=marca;
+        this.stock=stock;
+        this.existencia=existencia;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public boolean getExistencia() {
+        return existencia;
+    }
+    
+}
